5d3cd54828c9847eb2086120e7caafd2307f4e7d
Manuelinux <jomafersan14@gmail.com>
2025-12-26 11:15:50 -0500

    iniciando(too): comenzanso a aprender

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const cerati = require("./assets/cerati.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={cerati} style={{ width: 100, height: 100 }} />
+      <Image
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsA8cVPyYr_UZb1UVBTrcP9s4quRNax2ksIA&s",
+        }}
+        style={{
+          width: 100,
+          height: 200,
+          resizeMode: "contain",
+        }}
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
